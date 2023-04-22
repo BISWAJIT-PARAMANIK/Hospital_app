a3c3b630566bc4f3c6cb759f4f14a32df4a7cfd7
BISWAJIT-PARAMANIK <biswajitparamanik12@gmail.com>
2023-04-22 11:33:15 +0530

    first commit

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Nav from "./nav/Nav";
+import About from "./about/About";
+import Testimonial from "./testimonial/Testimonial";
+import Footer from "./footer/Footer";
+
+const MainPage = () => {
+  return (
+    <>
+      <Nav />
+      <About />
+      <Testimonial />
+      <Footer />
+    </>
+  );
+};
+
+export default MainPage;
