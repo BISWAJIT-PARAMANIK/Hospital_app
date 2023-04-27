5facd952c69a0593ea41eca4ef0a77da8927a285
BISWAJIT-PARAMANIK <biswajitparamanik12@gmail.com>
2023-04-27 11:31:51 +0530

    27/04/2023

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./home.css";
-<home className="css"></home>;
+import nurse from "../../images/nurse1.png";
+import doctor from "../../images/doctor1.png";
+import helpdesk from "../../images/can-i-help-you-.png";
+import appointment from "../../images/appointment.png";
+import emergency from "../../images/emergency.png";
+import service from "../../images/24-7-service.avif";
 
 const Home = () => {
   return (
@@ -23,6 +28,70 @@ const Home = () => {
               <a href="" className="main-btn mt-4 ms-3">
                 Learn more
               </a>
+            </div>
+            <div className="col-md-6 order-md-2 order-1 mb-md-0 mb-5">
+              <div className="top-right-sec">
+                <div className="animate-img">
+                  <img src={nurse} alt="aimg1" className="aimg1" />
+                  <img src={doctor} alt="aimg2" className="aimg2" />
+                </div>
+                <img
+                  src={helpdesk}
+                  alt="aimg3"
+                  className="img-fluid ms-xl-5 aimg3"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wrapper pb-0">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 col-sm-6 mb-4">
+              <div className="card text-center">
+                <div className="icon-box">
+                  <img
+                    src={appointment}
+                    alt="appointment_icon"
+                    className="homepageicons"
+                  />
+                </div>
+                <div>
+                  <h4>Easy Appointment</h4>
+                  <p>Book an appointment and feel the advance hospitality</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-6 mb-4">
+              <div className="card text-center">
+                <div className="icon-box">
+                  <img
+                    src={emergency}
+                    alt="emergency_icon"
+                    className="homepageicons"
+                  />
+                </div>
+                <div>
+                  <h4>Emergengy service</h4>
+                  <p>We will give you emergency services</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-6 mb-4">
+              <div className="card text-center">
+                <div className="icon-box">
+                  <img
+                    src={service}
+                    alt="service_icon"
+                    className="homepageicons"
+                  />
+                </div>
+                <div>
+                  <h4>24x7 Service</h4>
+                  <p>We are always open 24*7 at your service</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
