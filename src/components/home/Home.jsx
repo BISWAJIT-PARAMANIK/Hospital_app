cb9af8647386f73a0be9db1a49f5a1aad34630ea
BISWAJIT-PARAMANIK <biswajitparamanik12@gmail.com>
2023-04-27 12:47:37 +0530

    27th

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,14 +10,13 @@ import service from "../../images/24-7-service.avif";
 const Home = () => {
   return (
     <section id="home" className="home">
-      <div className="banner_wrapper warapper">
+      <div className="banner_wrapper wrapper">
         <div className="container">
           <div className="row align-items-center">
             <div className="col-md-6 order-md-1 order-2">
               <h3> Better life thoughts</h3>
               <h1>Better Hospitality</h1>
               <p>
-                {" "}
                 join us to a fun and friendly environment. Our professinals are
                 wroking so far to see smile on your face that you deservel! We
                 are dedicated to our duties.
